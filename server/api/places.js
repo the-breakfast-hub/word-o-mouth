a1c04cdc2fec4c06023f3340c6792de7fbe0fa6f
sam-peach <sampeachart@gmail.com>
2019-06-06 13:17:04 -0400

    /newPlace route in api is returning the correct data

--- server/api/places.js
+++ server/api/places.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Restaurant } = require("../db/index");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const axios = require("axios");
 
 router.put("/", async (req, res, next) => {
   try {
@@ -13,6 +14,18 @@ router.put("/", async (req, res, next) => {
       }
     });
     res.json(found);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/newPlace", async (req, res, next) => {
+  try {
+    const { data } = await axios.get(
+      "https://api.foursquare.com/v2/venues/search?ll=40.7047,74.0094&client_id=5IDUOTEW20UIMVSEBNT1UJCSCBXQQB4X55DLJDE0QCK23TKT&client_secret=QWUQ4HLZKU34TZNMSCHBMYP0NZ2TUFSQX0RPX4TOTWMZ0MPN&v=20190606"
+    );
+    console.log(">>> ", data);
+    res.json(data);
   } catch (error) {
     next(error);
   }
