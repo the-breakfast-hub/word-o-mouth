ff9d839d0f4c29f75a7d1e55c215821705e2eaf8
sam-peach <sampeachart@gmail.com>
2019-06-06 14:22:14 -0400

    The 'places' api can take a query to search specific places

--- server/api/places.js
+++ server/api/places.js
@@ -19,13 +19,20 @@ router.put("/", async (req, res, next) => {
   }
 });
 
-router.get("/newPlace", async (req, res, next) => {
+// Default informtion for the query search
+// 40.705132,-74.009258
+// ll=40.705132,-74.009258&radius=100&intent=broswe&categoryId=4bf58dd8d48988d14e941735
+
+router.get("/", async (req, res, next) => {
+  let newString = "";
+  for (const key in req.query) {
+    newString += `${key}=${req.query[key]}&`;
+  }
   try {
     const { data } = await axios.get(
-      "https://api.foursquare.com/v2/venues/search?ll=40.7047,74.0094&client_id=5IDUOTEW20UIMVSEBNT1UJCSCBXQQB4X55DLJDE0QCK23TKT&client_secret=QWUQ4HLZKU34TZNMSCHBMYP0NZ2TUFSQX0RPX4TOTWMZ0MPN&v=20190606"
+      `https://api.foursquare.com/v2/venues/search?${newString}client_id=5IDUOTEW20UIMVSEBNT1UJCSCBXQQB4X55DLJDE0QCK23TKT&client_secret=QWUQ4HLZKU34TZNMSCHBMYP0NZ2TUFSQX0RPX4TOTWMZ0MPN&v=20190606`
     );
-    console.log(">>> ", data);
-    res.json(data);
+    res.json(data.response);
   } catch (error) {
     next(error);
   }
